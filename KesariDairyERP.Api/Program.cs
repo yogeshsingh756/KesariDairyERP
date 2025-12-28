94cfac7a703e521d36ddb3d0c76de536f4548af3
yogeshsingh756 <ythakursingh98@gmail.com>
2025-12-28 18:05:53 +0530

    fix

--- KesariDairyERP.Api/Program.cs
+++ KesariDairyERP.Api/Program.cs
@@ -188,11 +188,11 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await RbacSeeder.SeedAsync(db);
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    await RbacSeeder.SeedAsync(db);
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
